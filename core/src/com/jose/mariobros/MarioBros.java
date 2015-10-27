7c082633c7a946802407cfa85ad5403eeeba7b26
Jose <chemita95@gmail.com>
2015-10-27 11:54:00 +0000

    Added playscreen variables, added Tiled map variables, added Box2d variables and debug lines to objects.

--- core/src/com/jose/mariobros/MarioBros.java
+++ core/src/com/jose/mariobros/MarioBros.java
@@ -6,18 +6,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.ApplicationListener;
 import com.jose.mariobros.Screens.PlayScreen;
-
 
 public class MarioBros extends Game {
 	public static final int V_WIDTH = 400;
-	public  static final int V_HEIGHT = 208;
-
+	public static final int V_HEIGHT = 208;
 
 	public SpriteBatch batch;
 
-	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -25,5 +21,7 @@ public class MarioBros extends Game {
 	}
 
 	@Override
-	public void render () { super.render(); }
+	public void render () {
+		super.render();
+	}
 }
